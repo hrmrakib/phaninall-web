c09e38333c48913e072aa32b87ef5279be39cdcb
hrmrakib <hrmrakibs@gmail.com>
2025-04-01 16:26:54 +0600

    fix svg issue

--- src/components/capability/CapabilityBanner.tsx
+++ src/components/capability/CapabilityBanner.tsx
@@ -10,43 +10,43 @@ export default function ContactBanner() {
     <div className='w-full'>
       {/* Main banner */}
       <div className='relative bg-[#FFFFFF] overflow-hidden'>
-        {/* <div className='container mx-auto'> */}
-        <div className='z-10 pb-8 bg-white'>
-          <div className='h-[612px] flex flex-col md:flex-row items-center justify-between'>
-            <div className='pl-0 md:pl-20 sm:text-center md:text-left'>
-              <button className='inline-block bg-[#FF6C0A] text-[32px] font-bold px-8 py-3 mb-6 text-[#FFFFFF]'>
-                Capability Statement
-              </button>
-              <h1 className='max-w-[600px] text-3xl md:text-5xl text-[#4D3F36] tracking-tight leading-tight'>
-                Driving Excellence in AI &{" "}
-                <span className='text-gray-900 font-semibold'>
-                  Machine Learning Education
-                </span>
-              </h1>
-            </div>
+        <div className='container mx-auto'>
+          <div className='z-10 pb-8 bg-white'>
+            <div className='h-[612px] flex flex-col md:flex-row items-center justify-between'>
+              <div className='sm:text-center md:text-left'>
+                <button className='inline-block bg-[#FF6C0A] text-[32px] font-bold px-8 py-3 mb-6 text-[#FFFFFF]'>
+                  Capability Statement
+                </button>
+                <h1 className='max-w-[600px] text-3xl md:text-5xl text-[#4D3F36] tracking-tight leading-tight'>
+                  Driving Excellence in AI &{" "}
+                  <span className='text-gray-900 font-semibold'>
+                    Machine Learning Education
+                  </span>
+                </h1>
+              </div>
 
-            <div className='relative z-40'>
-              <div
-                className={`relative transition-transform duration-300 ease-in-out ${
-                  isHovered ? "transform translate-y-[-8px]" : ""
-                }`}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                <div className='relative z-10'>
-                  <Image
-                    src='/capability/banner.svg'
-                    alt='Customer Support Agent'
-                    width={700}
-                    height={700}
-                    className='relative z-10'
-                  />
+              <div className='relative z-40'>
+                <div
+                  className={`relative transition-transform duration-300 ease-in-out ${
+                    isHovered ? "transform translate-y-[-8px]" : ""
+                  }`}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  <div className='relative z-10'>
+                    <Image
+                      src='/capability/banner.svg'
+                      alt='Customer Support Agent'
+                      width={700}
+                      height={700}
+                      className='relative z-10'
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
--- src/components/careers/CareerBanner.tsx
+++ src/components/careers/CareerBanner.tsx
@@ -11,35 +11,37 @@ export default function CareersBanner() {
       {/* Main banner */}
       <div className='relative bg-[#FFFFFF] overflow-hidden'>
         <div className='z-10 pb-8 bg-white'>
-          <div className='h-[678px] flex flex-col md:flex-row items-center justify-between'>
-            <div className='pl-0 md:pl-20 mt-6 md:mt-0 sm:text-center md:text-left '>
-              <button className='inline-block bg-[#FF6C0A] text-[32px] font-bold px-8 py-3 mb-6 text-[#FFFFFF]'>
-                Careers
-              </button>
-              <h1 className='max-w-[600px] text-3xl md:text-5xl text-[#4D3F36] tracking-tight leading-tight p-2 md:p-0'>
-                Join Our Mission to Shape the Future of{" "}
-                <span className='text-gray-900 font-semibold'>
-                  AI & Machine Learning
-                </span>
-              </h1>
-            </div>
+          <div className='container mx-auto'>
+            <div className='h-[678px] flex flex-col md:flex-row items-center justify-between'>
+              <div className='mt-6 md:mt-0 sm:text-center md:text-left '>
+                <button className='inline-block bg-[#FF6C0A] text-[32px] font-bold px-8 py-3 mb-6 text-[#FFFFFF]'>
+                  Careers
+                </button>
+                <h1 className='max-w-[600px] text-3xl md:text-5xl text-[#4D3F36] tracking-tight leading-tight p-2 md:p-0'>
+                  Join Our Mission to Shape the Future of{" "}
+                  <span className='text-gray-900 font-semibold'>
+                    AI & Machine Learning
+                  </span>
+                </h1>
+              </div>
 
-            <div className='mt-10 md:mt-0 relative'>
-              <div
-                className={`relative transition-transform duration-300 ease-in-out ${
-                  isHovered ? "transform translate-y-[-8px]" : ""
-                }`}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                <div className='relative z-10'>
-                  <Image
-                    src='/careers/banner.svg'
-                    alt='Customer Support Agent'
-                    width={700}
-                    height={700}
-                    className='relative z-10'
-                  />
+              <div className='relative'>
+                <div
+                  className={`relative transition-transform duration-300 ease-in-out ${
+                    isHovered ? "transform translate-y-[-8px]" : ""
+                  }`}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  <div className='relative z-10'>
+                    <Image
+                      src='/careers/banner.svg'
+                      alt='Customer Support Agent'
+                      width={700}
+                      height={700}
+                      className='relative z-10'
+                    />
+                  </div>
                 </div>
               </div>
             </div>
