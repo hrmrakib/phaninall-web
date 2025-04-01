faee6fcbe597983dc33bf55eac93699a95dc94ed
hrmrakib <hrmrakibs@gmail.com>
2025-04-01 16:12:38 +0600

    career page responsive

--- src/components/careers/JobCard.tsx
+++ src/components/careers/JobCard.tsx
@@ -33,7 +33,7 @@ export default function JobCard() {
   }
 
   return (
-    <main className='container mx-auto px-4 py-12'>
+    <main className='container mx-auto px-4 pb-12'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {jobs.map((job) => (
           <div
