3c61f2f5a6c2ffd622da17e985d14490f8fbf53b
hrmrakib <hrmrakibs@gmail.com>
2025-04-16 22:17:59 +0600

    kam sara 2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    domains: ["https://orangetechconsultants.com"], // or any external host
+  },
 };
 
 export default nextConfig;
